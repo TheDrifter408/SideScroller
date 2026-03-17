9e4d375b174f02724da71e01eb64fad93271ef38
TheDrifter408 <nurullahan408@gmail.com>
2026-03-17 23:58:25 +0600

    Added set and events for keys

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,64 @@
 import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
+import { GRAVITY } from './constants';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src=${viteLogo} class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+const ctx = canvas?.getContext('2d');
 
-<div class="ticks"></div>
+// Set to keep track of keys pressed
+const keys = new Set();
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src=${viteLogo} alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+// Listener to update set of keys pressed
+window.addEventListener('keydown', (event) => keys.add(event));
+window.addEventListener('keyup', (event) => keys.delete(event))
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+let player = {
+  x: 50,
+  y: 30,
+  width: 30,
+  height: 30,
+  frameX: 0,
+  maxFrame: 5,
+  fps: 10,
+  speed: 5,
+  moving: false,
+  facingLeft: false,
+  dy: 0,
+  jumpForce: 12,
+  grounded: false,
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function update() {
+  player.dy += GRAVITY;
+  player.y += player.dy;
+
+  // Ground Collision
+  if (player.y + player.height > canvas.height) {
+    player.y = canvas.height - player.height;
+    player.dy = 0;
+    player.grounded = true;
+  }
+
+}
+
+function draw() {
+  if (ctx) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+  }
+}
+
+window.addEventListener('keydown', (event) => {
+  if (event.code === 'Space' && player.grounded) {
+    player.dy -= player.jumpForce;
+    player.grounded = false;
+  }
+})
+
+function gameLoop() {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height); // 1. Clear
+  update(); // 2. Update
+  draw(); //  3. Draw
+  requestAnimationFrame(gameLoop); // 4. Repeat
+}
+
+gameLoop();
